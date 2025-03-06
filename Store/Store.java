cf1c577a2751517a30fb1c7197a34c17e4675553
Astra <139716057+wkeav@users.noreply.github.com>
2025-03-06 16:07:40 -0600

    Create Store.java

---
+++ Store/Store.java
@@ -0,0 +1,16 @@
+public class Store {
+  // instance fields
+  String productType;
+    
+  // constructor method
+  public Store(String product) {
+    productType = product;
+  }
+    
+  // Add advertise method below
+    
+  public void advertise(){
+
+  }
+
+}
