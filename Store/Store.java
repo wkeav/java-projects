c78848c90fef561baaec3f35e961dfd9a6b16d4c
Astra <astra.k.nguyen05@outlook.com>
2025-03-07 10:34:05 -0600

    Updated Store.java with new functionality

---
+++ Store/Store.java
@@ -0,0 +1,23 @@
+package Store;
+
+public class Store {
+    // instance fields
+    String productType;
+    
+    // constructor method
+    public Store(String product) {
+        productType = product;
+    }
+
+    // advertise method
+    public void advertise() {
+        String message = "Selling " + productType + "!";
+        System.out.println(message);
+    }
+    
+    // greetCustomer() method
+    public void greetCustomer(String customerName){
+        System.out.println("Welcome to the store, " + customerName + "!");
+    }
+}
+
