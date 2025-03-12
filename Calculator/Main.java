9d8b1ec987fd80683cae37567dc7606a424cbdb4
Astra <astra.k.nguyen05@outlook.com>
2025-03-12 12:16:14 -0500

    Update codes

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -3,21 +3,68 @@ package Calculator;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
     Calculator calculator = new Calculator();
     Scanner scanner = new Scanner(System.in);
     
     boolean running = true;
 
-    // while(running){
-    //     System.out.println("\n===== CALCULATOR MENU =====");
-    //     System.out.println("1. Basic Operations (+, -, *, /, %)");
-    //     System.out.println("2. Exit");
+    while(running){
+        System.out.println("\n===== CALCULATOR MENU =====");
+        System.out.println("1. Basic Operations");
+        System.out.println("2. Scientific Operations");
+        System.out.println("Exit");
+        
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
 
-    //     int choice = scanner.nextInt(); 
+        switch(choice){
+            case 1:
+                performBasicOperation(calculator, scanner);
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
 
-    //     switch(choice){
-    //         case 1: 
+    //helper methods
+    private static void performBasicOperation(Calculator calculator, Scanner scanner){
+        System.out.println("\n--- Basic Operations ---");
+        System.out.println("1. Addition (+) ");
+        System.out.println("2. Subtraction (-) ");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
 
-    //     }
-    // }
+        System.out.println("Enter your choice of operation: ");
+        int choice = scanner.nextInt(); 
+
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        switch(choice){
+            case 1: 
+                System.out.println(calculator.add(num1,num2));
+                break;
+            case 2:
+                System.out.println(calculator.subtract(num1, num2));
+                break;
+            case 3:
+                System.out.println(calculator.multiply(num1, num2));
+                break;
+            case 4:
+                try {
+                    System.out.println("Result: " + calculator.divide(num1, num2));
+                } catch (ArithmeticException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+                break;
+            default:
+                System.out.println("Invalid operation. Try again.");
+        }
+    }
+
 }
+
