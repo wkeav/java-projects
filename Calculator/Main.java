cd4a994aa0c47a16e7813327ad5274087f1686d5
Astra <astra.k.nguyen05@outlook.com>
2025-03-14 11:37:46 -0500

    update codes & fix errors

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -77,58 +77,6 @@ public class Main {
         }
     }
 
-<<<<<<< HEAD
-        private static void performScientificOperation(Calculator calculator, Scanner scanner){
-            System.out.println("\\n--- Scientific Operations ---");
-            System.out.println("1. Modulo");
-            System.out.println("2. Square");
-            System.out.println("3. Square Root");
-            System.out.println("4. Power");
-            System.out.println("5. Sin/cos/tan");
-            System.out.println("6. Log");
-            System.out.println("7. Ln");
-
-            System.out.println("Enter your choice of operation: ");
-            int choice = scanner.nextInt();
-            
-            switch(choice){
-                case 1: 
-                    System.out.println("Enter first number: ");
-                    double num1 = scanner.nextDouble();
-                    System.out.println("Enter second number: ");
-                    double num2 = scanner.nextDouble();
-
-                    System.out.println(calculator.modulo(num1, num2));
-                    break;
-                case 2:
-                    System.out.println("Enter number: ");
-                    double numSquare = scanner.nextDouble();
-                    System.out.println(calculator.square(numSquare));
-                    break;
-                case 3:
-                    System.out.println("Enter number: ");
-                    double numDouble = scanner.nextDouble();
-                    System.out.println(calculator.squareRoot(numDouble));
-                case 4:
-                    System.out.println("Enter first number: ");
-                    double num1Power = scanner.nextDouble();
-                    System.out.println("Enter second number: ");
-                    double num2Power= scanner.nextDouble();
-
-                    System.out.println(calculator.power(num1Power,num2Power));
-                    break;
-                case 5:
-                    System.out.println("Choose operations");
-                    System.out.println("1. Sin");
-                    System.out.println("2. Cos");
-                    System.out.println("3. Tan");
-
-                    int choice2 = scanner.nextInt();
-
-            }
-
-        }
-=======
     private static void performScientificOperation(Calculator calculator, Scanner scanner){
         System.out.println("\n--- Scientific Operations ---");
         System.out.println("1. Square");
@@ -217,6 +165,5 @@ public class Main {
                 System.out.println("Error: " + e.getMessage());
         }
     }
->>>>>>> ef752c0 (update codes)
 }
 
